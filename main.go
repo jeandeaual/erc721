6855a8c089381f0ea562529af19aca932a052f73
Alexis Jeandeau <alexis.jeandeau@gmail.com>
2019-07-10 17:10:04 +0900

    Deploy the contract to the Ropsten network

--- main.go
+++ main.go
@@ -3,54 +3,95 @@
 package main
 
 import (
+	"context"
+	"crypto/ecdsa"
+	"flag"
 	"fmt"
 	"log"
 	"math/big"
+	"path/filepath"
+	"os"
 
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
-	"github.com/ethereum/go-ethereum/accounts/abi/bind/backends"
-	"github.com/ethereum/go-ethereum/core"
 	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/ethclient"
+)
+
+const (
+	defaultEndpoint = "https://ropsten.infura.io/v3/bdffc1723bc8468b8bf8879d54a10cbc"
+	defaultGasPrice = 0
+	defaultGasLimit = 300000
 )
 
 func main() {
-	// Generate a new random account and a funded simulator
-	key, _ := crypto.GenerateKey()
-	auth := bind.NewKeyedTransactor(key)
+	var (
+		endpoint string
+		gasPrice int
+		gasLimit int
+	)
 
-	sim := backends.NewSimulatedBackend(core.GenesisAlloc{auth.From: {Balance: big.NewInt(10000000000)}}, 10000000)
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage: %s PRIVATE_KEY\n\nFlags:\n", filepath.Base(os.Args[0]))
+		flag.PrintDefaults()
+	}
 
-	// Deploy a token contract on the simulated blockchain
-	addr, _, contract, err := DeployERC721(auth, sim)
+	flag.StringVar(&endpoint, "pattern", defaultEndpoint, "Connection endpoint")
+	flag.IntVar(&gasPrice, "gasPrice", defaultGasPrice, "Gas price for the deployment (defaults to the suggested price)")
+	flag.IntVar(&gasLimit, "gasLimit", defaultGasLimit, "Gas limit for the deployment")
+
+	flag.Parse()
+
+	key := flag.Arg(0)
+	if len(key) == 0 {
+		fmt.Fprintf(os.Stderr, "No private key provided\n")
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	log.Println("Dialing", endpoint)
+
+	client, err := ethclient.Dial(endpoint)
 	if err != nil {
-		log.Fatalf("Failed to deploy new token contract: %v", err)
+		log.Fatal(err)
+	}
+
+	privateKey, err := crypto.HexToECDSA(key)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	publicKey := privateKey.Public()
+	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+	if !ok {
+		log.Fatal("Cannot assert type: publicKey is not of type *ecdsa.PublicKey")
+	}
+
+	fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
+	nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	suggestedGasPrice, err := client.SuggestGasPrice(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Suggested gas price:", suggestedGasPrice)
+
+	auth := bind.NewKeyedTransactor(privateKey)
+	auth.Nonce = big.NewInt(int64(nonce))
+	auth.Value = big.NewInt(0)
+	auth.GasLimit = uint64(gasLimit)
+	if gasPrice == defaultGasPrice {
+		auth.GasPrice = suggestedGasPrice
+	} else {
+		auth.GasPrice = big.NewInt(int64(gasPrice))
+	}
+
+	addr, tx, _, err := DeployERC721(auth, client)
+	if err != nil {
+		log.Fatalf("Failed to deploy new contract: %v", err)
 	}
 	fmt.Println("Contract deployed at address", addr.String())
-
-	// Commit all pending transactions in the simulator and print the names again
-	sim.Commit()
-
-	// Check the balance
-	balance, err := contract.BalanceOf(nil, auth.From)
-	if err != nil {
-		log.Fatalf("Failed to retrieve balance of %v: %v", auth.From, err)
-	}
-
-	fmt.Println("Balance:", *balance)
-
-	// Mint a new token
-	tx, err := contract.Mint(nil, auth.From, big.NewInt(123))
-	if err != nil {
-		log.Fatalf("Failed to mint new token to %v: %v", auth.From, err)
-	}
-
-	fmt.Println("Token minted, transaction", tx.Hash().String())
-
-	// Retrieve the owner of the token
-	owner, err := contract.OwnerOf(nil, big.NewInt(1))
-	if err != nil {
-		log.Fatalf("Failed to retrieve token owner: %v", err)
-	}
-
-	fmt.Println("Token owner:", owner.String())
+	fmt.Println("Transaction:", tx.Hash().String())
 }
